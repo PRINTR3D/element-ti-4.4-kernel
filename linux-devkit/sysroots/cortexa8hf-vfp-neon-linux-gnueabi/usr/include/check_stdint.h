73bf29655e2c5639b6056b733ecd15c6558a5cdc
Peter Oostewechel <petero@scintilla.utwente.nl>
2016-06-22 13:24:44 +0200

    Added toolchain

---
+++ linux-devkit/sysroots/cortexa8hf-vfp-neon-linux-gnueabi/usr/include/check_stdint.h
@@ -0,0 +1,9 @@
+#ifndef _CHECK_CHECK_STDINT_H
+#define _CHECK_CHECK_STDINT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "check 0.9.14"
+/* generated using gnu compiler arm-linux-gnueabihf-gcc (Linaro GCC 4.9-2015.05) 4.9.3 20150413 (prerelease) */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
---
+++ linux-devkit/sysroots/cortexa8hf-vfp-neon-linux-gnueabi/usr/include/liboil-0.3/liboil/liboil-stdint.h
@@ -0,0 +1,9 @@
+#ifndef _LIBOIL_LIBOIL_LIBOIL_STDINT_H
+#define _LIBOIL_LIBOIL_LIBOIL_STDINT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "liboil 0.3.17"
+/* generated using gnu compiler arm-linux-gnueabihf-gcc (Linaro GCC 4.9-2015.05) 4.9.3 20150413 (prerelease) */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
